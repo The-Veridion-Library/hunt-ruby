d30d5234a008f32c900b9bdc02fe3d901a315cfb
TheAwesomeAJ <theawesomeaj.dev@gmail.com>
2026-03-06 13:50:02 -0500

    Added core files

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
     RubyPlugin(),
+    tailwindcss(),
     svelte()
   ]
 })
